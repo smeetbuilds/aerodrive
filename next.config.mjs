c971092ab0ffb9a9a48d6db2f0aa3eca0414a5cc
Smeet Ghori <72125852+smeetbuilds@users.noreply.github.com>
2026-07-08 12:58:52 +0530

    fix: allow Next hydration inline scripts

--- next.config.mjs
+++ next.config.mjs
@@ -16,7 +16,7 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-eval' 'wasm-unsafe-eval'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' 'wasm-unsafe-eval'",
               "style-src 'self' 'unsafe-inline'",
               "img-src 'self' data: blob:",
               "font-src 'self' data:",
